6b7fe2268a7133fead27eaaf433f285f6e7f86d6
Allen Dai <allendai0114@gmail.com>
2022-08-02 17:36:55 -0400

    q37 completed

--- q37/src/main.rs
+++ q37/src/main.rs
@@ -2,45 +2,111 @@ use std::collections::HashSet;
 
 struct Solution;
 impl Solution {
-    pub fn get_cell(row: usize, col: usize) -> usize {
-        row / 3 * 3 + col / 3
-    }
-
     pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
-        let mut row_contain: Vec<HashSet<u32>> = vec![HashSet::new(); 9];
-        let mut col_contain: Vec<HashSet<u32>> = vec![HashSet::new(); 9];
-        let mut cell_contain: Vec<HashSet<u32>> = vec![HashSet::new(); 9];
+        fn get_cell(row: usize, col: usize) -> usize {
+            (row / 3) * 3 + col / 3
+        }
+
+        fn get_next_row(row: usize, col: usize) -> usize {
+            row + (col + 1) / 9
+        }
+
+        fn get_next_col(col: usize) -> usize {
+            (col + 1) % 9
+        }
+
+        pub fn get_next_pos(
+            board: &Vec<Vec<char>>,
+            mut row: usize,
+            mut col: usize,
+        ) -> (usize, usize) {
+            while row != 9 {
+                if board[row][col] == '.' {
+                    return (row, col);
+                }
+                row = get_next_row(row, col);
+                col = get_next_col(col);
+            }
+            return (9, 0);
+        }
+
+        fn backtrack(
+            board: &mut Vec<Vec<char>>,
+            row_contain: &mut HashSet<(usize, u32)>,
+            col_contain: &mut HashSet<(usize, u32)>,
+            cell_contain: &mut HashSet<(usize, u32)>,
+            r: usize,
+            c: usize,
+        ) -> bool {
+            let (R, C) = get_next_pos(&board, r, c);
+
+            if R == 9 {
+                return true;
+            }
+            let CL = get_cell(R, C);
+
+            let mut used = HashSet::new();
+            for n in 1..10 {
+                let x = n as u32;
+                let rr = (R, x);
+                let cc = (C, x);
+                let ccl = (CL, x);
+                if row_contain.contains(&rr) {
+                    used.insert(n);
+                }
+                if col_contain.contains(&cc) {
+                    used.insert(n);
+                }
+                if cell_contain.contains(&ccl) {
+                    used.insert(n);
+                }
+            }
+            if used.len() == 9 {
+                return false;
+            }
+
+            for idx in 1..10 {
+                if !used.contains(&idx) {
+                    board[R][C] = ((idx + 48) as u8) as char;
+
+                    row_contain.insert((R, idx));
+                    col_contain.insert((C, idx));
+                    cell_contain.insert((CL, idx));
+                    if backtrack(board, row_contain, col_contain, cell_contain, R, C) {
+                        return true;
+                    }
+
+                    row_contain.remove(&(R, idx));
+                    col_contain.remove(&(C, idx));
+                    cell_contain.remove(&(CL, idx));
+                }
+            }
+
+            board[R][C] = '.';
+            return false;
+        }
+
+        let mut row_contain: HashSet<(usize, u32)> = HashSet::with_capacity(9 * 9);
+        let mut col_contain: HashSet<(usize, u32)> = HashSet::with_capacity(9 * 9);
+        let mut cell_contain: HashSet<(usize, u32)> = HashSet::with_capacity(9 * 9);
         for r in 0..9 {
             for c in 0..9 {
                 if let Some(num) = board[r][c].to_digit(10) {
-                    row_contain[r].insert(num);
-                    col_contain[c].insert(num);
-                    cell_contain[Self::get_cell(r, c)].insert(num);
+                    row_contain.insert((r, num));
+                    col_contain.insert((c, num));
+                    cell_contain.insert((get_cell(r, c), num));
                 }
             }
         }
 
-        Self::backtrack(
+        backtrack(
             board,
             &mut row_contain,
             &mut col_contain,
             &mut cell_contain,
-            (0, 0),
+            0,
+            0,
         );
-
-        println!(
-            "\n\n{:?}\n\n{:?}\n\n{:?}\n",
-            row_contain, col_contain, cell_contain
-        );
-    }
-
-    pub fn backtrack(
-        board: &mut Vec<Vec<char>>,
-        row_contain: &mut Vec<HashSet<u32>>,
-        col_contain: &mut Vec<HashSet<u32>>,
-        cell_contain: &mut Vec<HashSet<u32>>,
-        pos: (usize, usize),
-    ) {
     }
 
     pub fn test() {
